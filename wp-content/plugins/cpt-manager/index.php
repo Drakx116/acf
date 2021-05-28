190b427443935b4a107bd93b95fb2535c46470f7
Drakx116 <alexandre2.guyot@gmail.com>
2021-05-28 15:14:19 +0200

    Reservation - Init CPT

--- wp-content/plugins/cpt-manager/index.php
+++ wp-content/plugins/cpt-manager/index.php
@@ -7,6 +7,10 @@
  * Author URI: https://github.com/Drakx116
  */
 
+
+
+// MAIN COURSES
+
 add_action('init', 'init_main_course_cpt', 0);
 
 function init_main_course_cpt() {
@@ -86,3 +90,64 @@ if (function_exists('acf_add_options_page')) {
         'position' => 10
     ]);
 }
+
+
+
+// RESERVATION
+
+function init_reservation_cpt() {
+    $labels = [
+        'name'                  => 'Reservations',
+        'singular_name'         => 'Reservation',
+        'menu_name'             => 'Reservation',
+        'name_admin_bar'        => 'Reservation',
+        'archives'              => 'Reservation Archives',
+        'attributes'            => 'Reservation Attributes',
+        'parent_item_colon'     => 'Parent Reservation :',
+        'all_items'             => 'All Reservations',
+        'add_new_item'          => 'Add New Reservation',
+        'add_new'               => 'Add New',
+        'new_item'              => 'New Reservation',
+        'edit_item'             => 'Edit Reservation',
+        'update_item'           => 'Update Reservation',
+        'view_item'             => 'View Reservation',
+        'view_items'            => 'View Reservations',
+        'search_items'          => 'Search Reservation',
+        'not_found'             => 'Not found',
+        'not_found_in_trash'    => 'Not found in Trash',
+        'featured_image'        => 'Featured Image',
+        'set_featured_image'    => 'Set featured image',
+        'remove_featured_image' => 'Remove featured image',
+        'use_featured_image'    => 'Use as featured image',
+        'insert_into_item'      => 'Insert into Reservation',
+        'uploaded_to_this_item' => 'Uploaded to this Reservation',
+        'items_list'            => 'Reservations list',
+        'items_list_navigation' => 'Reservations list navigation',
+        'filter_items_list'     => 'Filter Reservation list'
+    ];
+
+    $args = [
+        'label'                 => 'Reservation',
+        'description'           => 'Event Reservation',
+        'labels'                => $labels,
+        'supports'              => ['title', 'editor'],
+        'hierarchical'          => false,
+        'public'                => true,
+        'show_ui'               => true,
+        'show_in_menu'          => true,
+        'menu_position'         => 25,
+        'menu_icon'             => 'dashicons-phone',
+        'show_in_admin_bar'     => true,
+        'show_in_nav_menus'     => true,
+        'can_export'            => true,
+        'has_archive'           => true,
+        'exclude_from_search'   => false,
+        'publicly_queryable'    => true,
+        'capability_type'       => 'page'
+    ];
+
+    register_post_type( 'reservation', $args );
+
+}
+
+add_action( 'init', 'init_reservation_cpt', 0 );
